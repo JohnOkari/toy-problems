7a7aed3fbcd7e1d6a47114f12a56afcf503c6f98
JohnOkari <johnnokari@gmail.com>
2022-12-08 14:10:27 +0300

    Done with assignment

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -1,23 +1,62 @@
-
-
-function netSalaryCalculator(tax){
-   // net income 
-    let income = parseFloat($("income"). value);
-    $("tax").value = calcPayee(income);
-    console.log(tax);
-    function calcPayee(ksh){
-        let calculate = 0;
-        if (ksh > 32000){
-            tax = (ksh ) * 0.3
-        }  else if (ksh <= 31999 && ksh >= 24000){
-            tax = (ksh)* 0.25
-        } else {
-            tax = (ksh)*0.1
+function netSalaryCalculator(gross,benefits){
+    // net income 
+        let netSalary = (gross + benefits) - (totalDeductions);
+        let nhif;
+        let nssf = 200;
+        let payee;
+        let totalDeductions = (nhif + payee + nssf);
+  
+  //calculating employee nhif
+        if (taxablePay > 23999){
+            return taxablePay;
+        }else{
+            if(taxablePay > 99999){
+                nhif = 1700;
+            }else if (taxablePay > 89999){
+                nhif = 1600;
+            }else if (taxablePay > 79999){
+                nhif = 1500;
+            }else if (taxablePay > 69999){
+                nhif = 1400;
+            }else if (taxablePay > 59999){
+                nhif = 1300;
+            }else if (taxablePay > 49999){
+                nhif = 1200;
+            }else if (taxablePay > 44999){
+                nhif = 1100;
+            }else if (taxablePay > 34999){
+                nhif = 1000;
+            }else if (taxablePay > 29999){
+                nhif = 900;
+            }else if (taxablePay > 24999){
+                nhif = 850;
+            }else if (taxablePay > 19999){
+                nhif = 750;
+            }else if (taxablePay > 14999){
+                nhif = 600;
+            }else if (taxablePay > 11999){
+                nhif = 500;
+            }else if (taxablePay > 7999){
+                nhif = 400;
+            }else if (taxablePay > 5999){
+                nhif = 300;
+            }else{
+                nhif = 150
+            }
+  //calaculating payee
+  
+        if (taxablePay>32333){
+            payee = 240000 * 0.1 + 8333 * 0.25 + (taxablePay - 32333) * 0.3;
+        }else if (taxablePay >= 24000){
+            payee = 24000 * 0.1 + (taxablePay - 24000) * 0.25;
+        }else if (taxablePay > 23999){
+            payee = 24000 * 0.1 ;
         }
-        //returns tax
-        return tax;
-    }  
-}
-netSalaryCalculator()
-
-// console.log(netSalaryCalculator())
+  // returning the netsalary
+  
+        return netSalary
+    }
+  
+  }
+  
+  // console.log(netSalaryCalculator())
